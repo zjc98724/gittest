b4d7e1198be5e62384d3e1ea7c9d099c8aa0fa0d
zjc98724 <zjc98724@qq.com>
2016-01-21 20:49:17 +0800

    修改输出内容

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -3,7 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git3");
+		System.out.println("Hello Git debug");
+		System.out.println("I am create a new Branch");
 	}
 
 }
