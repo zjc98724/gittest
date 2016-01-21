41cd3b4b91bfefa2d1586e53b9b4da8a84592a60
zjc98724 <zjc98724@qq.com>
2016-01-21 20:52:51 +0800

    修改new为old

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git debug");
-		System.out.println("I am create a new Branch");
+		System.out.println("I am create a old Branch");
 	}
 
 }
